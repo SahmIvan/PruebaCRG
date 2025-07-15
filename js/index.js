0c174c2ae2a70dd4ecda475646aa0ab6412d9d39
ivan <ivan_gtm@outlook.com>
2025-07-14 19:13:31 -0600

    Prueba FadeIN

--- js/index.js
+++ js/index.js
@@ -4,4 +4,9 @@
         setTimeout(() => {
             window.location.href = './landing.html';
         }, 1000); // Espera 2 segundos para redirigir
-        });
+    });
+
+// Fade-in effect for the landing page
+    document.addEventListener('DOMContentLoaded', function() {
+        document.body.classList.add('fade-in');
+    });
