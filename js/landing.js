968cf312d4322eed8f2cd7de20e1a1c501b2a29b
ivan <ivan_gtm@outlook.com>
2025-09-28 21:04:37 -0600

    Se cambia formato de imagenes

--- js/landing.js
+++ js/landing.js
@@ -2,5 +2,5 @@ window.addEventListener('DOMContentLoaded', () => {
     // Esperar un par de milisegundos para que el estilo inicial (opacity:0) se aplique
     setTimeout(() => {
       document.body.classList.add('fade-in');
-    }, 50);
+    }, 300);
   });
